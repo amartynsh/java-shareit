5f72a184f6bf67a7ce8530016e6bdb5e8554412d
amartynsh <161699613+amartynsh@users.noreply.github.com>
2024-09-23 13:39:30 +0300

    Правка стиля

--- src/main/java/ru/practicum/shareit/item/dto/CommentMapper.java
+++ src/main/java/ru/practicum/shareit/item/dto/CommentMapper.java
@@ -5,14 +5,15 @@ import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.User;
 
 public class CommentMapper {
-    public static Comment toComment(CommentRequestDto commentRequestDto, Item item, User author){
+    public static Comment toComment(CommentRequestDto commentRequestDto, Item item, User author) {
         Comment comment = new Comment();
         comment.setText(commentRequestDto.getText());
         comment.setItem(item);
         comment.setAuthor(author);
         return comment;
     }
-    public static CommentResponseDto toCommentResponseDto(Comment comment){
+
+    public static CommentResponseDto toCommentResponseDto(Comment comment) {
         CommentResponseDto commentResponseDto = new CommentResponseDto();
         commentResponseDto.setId(comment.getId());
         commentResponseDto.setText(comment.getText());
