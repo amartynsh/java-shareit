f52711029d8fe2b7b8a6f1998d9a3e9a615b9cb4
amartynsh <161699613+amartynsh@users.noreply.github.com>
2024-10-01 14:41:18 +0300

    Промежуточный коммит, добавляю тесты

--- server/src/test/java/ru/practicum/shareit/item/ItemControllerTest.java
+++ server/src/test/java/ru/practicum/shareit/item/ItemControllerTest.java
@@ -3,34 +3,21 @@ package ru.practicum.shareit.item;
 import org.junit.jupiter.api.Test;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.MediaType;
+
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import ru.practicum.shareit.item.dto.ItemDto;
-import ru.practicum.shareit.request.ItemRequest;
+
 import ru.practicum.shareit.request.dto.ItemRequestDto;
 import ru.practicum.shareit.request.dto.ItemRequestResponseDto;
-import ru.practicum.shareit.user.UserController;
+
 import ru.practicum.shareit.user.UserService;
-import ru.practicum.shareit.user.dto.UserDto;
 
-import java.nio.charset.StandardCharsets;
-
-import static org.hamcrest.Matchers.is;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-
-import static org.junit.jupiter.api.Assertions.*;
 @ExtendWith(MockitoExtension.class)
 class ItemControllerTest {
 
@@ -60,15 +47,7 @@ class ItemControllerTest {
 
     @Test
     void itemShouldBeCreated() throws Exception {
-        itemRequestDto = new ItemRequestDto(null, "Test Name", "Test Description",true, null);
-        itemRequestResponseDto = new ItemRequestResponseDto(itemDto);
 
-        when(userService.findById(any())).thenReturn(new UserDto(1L, "Test User", "test@test.com", "password"));
-        when(controller.createItem(any())).thenReturn(itemRequestResponseDto);
-
-        mvc.perform(post("/items")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(mapper.writeValueAsString(itemRequestDto)))
     }
 
     @Test
