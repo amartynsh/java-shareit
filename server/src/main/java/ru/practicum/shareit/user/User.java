6b6dd75353f88b6ad8226e3f66988ac9f88ee29c
amartynsh <161699613+amartynsh@users.noreply.github.com>
2024-10-01 11:23:24 +0300

    Мелкие правки

--- server/src/main/java/ru/practicum/shareit/user/User.java
+++ server/src/main/java/ru/practicum/shareit/user/User.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.user;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
--- server/src/main/java/ru/practicum/shareit/user/UserController.java
+++ server/src/main/java/ru/practicum/shareit/user/UserController.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.user;
 
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.Email;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
